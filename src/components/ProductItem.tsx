16077e61265417be2f1c52303332e26aa4ae1d22
Caio Lemec <caio.lemec@gmail.com>
2021-08-05 17:03:15 -0300

    first commit

---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,15 @@
+interface ProductItemsProps {
+    product: {
+        id:number;
+        price: number;
+        title: string;
+    }
+}
+
+export function ProductItem ({product}: ProductItemsProps) {
+    return (
+        <div>
+            {product.title} - <strong>{product.price}</strong>
+        </div>
+    );
+}
